eb4824dd0529eb9073328277de5252d627d643e4
SashaOcheev <OcheevSasha@gmail.com>
2017-07-10 20:41:42 +0300

    -Рабочая модель игры -Блок

---
+++ Assets/Scripts/Direct.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts
+{
+    public enum Direct
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+}
